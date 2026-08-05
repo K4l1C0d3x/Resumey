7c70e76ec2eb52ceedfac6773613530d237421d0
CODEKJ777 <kalidaskj17@gmail.com>
2026-08-05 22:05:53 +0530

    style: add top padding to sidebar header

--- Resumey-main/components/app-sidebar.tsx
+++ Resumey-main/components/app-sidebar.tsx
@@ -70,7 +70,7 @@ const data = {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
+      <SidebarHeader className="pt-6 pb-2 px-4">
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton
